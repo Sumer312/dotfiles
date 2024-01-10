71df28bac9050315921d77d4aa0bf073f1f01f59
Sumer312 <sumergandhi79@gmail.com>
2024-01-11 02:45:00 +0530

    Dunst

--- bashScripts/lock.sh
+++ bashScripts/lock.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-i3lockmore --blur -e --lock-icon ~/i3lockmore/lock.png
-# if [[ -z $(xrandr | grep "HDMI-A-0 disconnected") ]]; then
-# 	i3lock -e --image ~/Pictures/debianLockScreen.png
-# else
-# 	i3lock -e --image ~/Pictures/Wallpapers/debianWallpaper.png
-# fi
+if [ -z "$(xrandr | grep "HDMI-A-0 disconnected")" ]; then
+  i3lock -n -e -i ~/Pictures/debianLockScreen.png
+else
+  i3lock -n -e -i ~/Pictures/Wallpapers/debianWallpaper.png
+fi
