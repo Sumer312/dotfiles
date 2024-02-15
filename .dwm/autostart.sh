957786bce164e66a2ea77d241280ce0a97d06b9a
Sumer312 <sumergandhi79@gmail.com>
2024-02-15 22:29:20 +0530

    lxpolkit

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+for i in $(ps aux | awk '/status.sh/' | awk '/\/bin\/sh/{print $2}')
+do
+  kill -9 "$i"
+done
+
 for i in $(ps aux | awk '/autostart.sh/' | awk '/\/bin\/sh/{print $2}')
 do
   if [ "$i" -eq "$$" ]; then
@@ -8,16 +13,12 @@ do
   kill -9 "$i"
 done
 
-for i in $(ps aux | awk '/status.sh/' | awk '/\/bin\/sh/{print $2}')
-do
-  kill -9 "$i"
-done
-
 dunst &
-/home/sumer/Documents/bashScripts/startupSound.sh &
+mpv --no-video /home/sumer/Music/startup/psp_startup.mp3 &
+xautolock -time 8 -locker /home/sumer/Documents/bashScripts/lock.sh -notify 20 -notifier /home/sumer/Documents/bashScripts/lockNotification.sh &
 /home/sumer/Documents/bashScripts/displayScript.sh &
 /home/sumer/Documents/bashScripts/touchpad.sh &
 /home/sumer/Documents/bashScripts/status.sh &
-xautolock -time 8 -locker /home/sumer/Documents/bashScripts/lock.sh -notify 20 -notifier /home/sumer/Documents/bashScripts/lockNotification.sh &
+lxpolkit &
 nitrogen --set-zoom-fill --random /home/sumer/Pictures/Wallpapers/ 
 compton --config /home/sumer/.config/compton/compton.conf
